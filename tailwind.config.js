2ba5dffd7ac98f39c0e9319a0fba0546d026029a
AriyanSep0_0 <ariansepahi.v8@gmail.com>
2024-08-20 12:04:52 +0330

    :tada: add styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Outfit"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Young Serif"', ...defaultTheme.fontFamily.serif],
+      },
+      screen: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        stone: {
+          100: "hsl(30, 54%, 90%)",
+          150: "hsl(30, 18%, 87%)",
+          600: "hsl(30, 10%, 34%)",
+          900: "hsl(24, 5%, 18%)",
+        },
+        brown: {
+          800: "hsl(14, 45%, 36%)",
+        },
+        rose: {
+          50: "hsl(330, 100%, 98%)",
+          800: "hsl(332, 51%, 32%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
